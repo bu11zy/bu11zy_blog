2f08888284f7514ac32f79b9a759caea6827ce73
bu11zy <bu11zy@yandex.ru>
2022-09-13 08:16:05 +0300

    v.0.0.8

--- bu11zy_blog/ablog/urls.py
+++ bu11zy_blog/ablog/urls.py
@@ -1,6 +1,6 @@
 from unicodedata import name
 from django.urls import path
-from .views import AddPostView, HomeView, ArticleDetailView, UpdatePostView
+from .views import AddPostView, HomeView, ArticleDetailView, UpdatePostView, DeletePostView
 
 
 urlpatterns = [
@@ -8,4 +8,5 @@ urlpatterns = [
     path('article/<int:pk>', ArticleDetailView.as_view(), name='article_detail'),
     path('add_post/', AddPostView.as_view(), name='add_post'),
     path('article/edit/<int:pk>', UpdatePostView.as_view(), name='update_post'),
+    path('article/<int:pk>/remove', DeletePostView.as_view(), name='delete_post'),
 ]
--- bu11zy_blog/ablog/views.py
+++ bu11zy_blog/ablog/views.py
@@ -1,11 +1,13 @@
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from .forms import PostForm, EditForm
+from django.urls import reverse_lazy
 
 
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
+    ordering = ['-id']
     
 
 class ArticleDetailView(DetailView):
@@ -23,3 +25,9 @@ class UpdatePostView(UpdateView):
     model = Post
     template_name = 'update_post.html'
     form_class = EditForm
+    
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
+    success_url = reverse_lazy('home')
